c5ee45128e1b24dee0abd78a55468c6ff154fb38
Daria Kravtsova <kravcova.dasha@bk.ru>
2019-12-20 23:17:56 -0700

    Updates

---
+++ precache-manifest.bbafe501bce74c680339556ae49e73a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-list-chat-rooms/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07ff57df04eb6085bf6a",
+    "url": "/my-app-list-chat-rooms/static/js/runtime~main.7f889392.js"
+  },
+  {
+    "revision": "1505be49179ef31b4d47",
+    "url": "/my-app-list-chat-rooms/static/js/main.2adaa034.chunk.js"
+  },
+  {
+    "revision": "7f566e329a65c75567c2",
+    "url": "/my-app-list-chat-rooms/static/js/2.9e4e18cf.chunk.js"
+  },
+  {
+    "revision": "1505be49179ef31b4d47",
+    "url": "/my-app-list-chat-rooms/static/css/main.cc836f88.chunk.css"
+  },
+  {
+    "revision": "8b9d7bb3530750e0f40f06f7821335ed",
+    "url": "/my-app-list-chat-rooms/index.html"
+  }
+];
